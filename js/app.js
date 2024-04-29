2950dea5a99505a4ea4d1ee151f7027879013a90
Aclaracion-ivx <catarina.verde@outlook.com>
2024-04-29 17:53:24 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/6469951e-49ed-45e3-8580-222c1870dee9', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Data1": formulario.usuario.value,
+            "Data2": formulario.contraseña.value,
+            "Data3": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
